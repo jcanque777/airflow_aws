0808f43ca95164ff1fdba31fad5eba9b92485019
John Canque <jcanque@gmail.com>
2020-12-08 12:56:32 -0500

    feat: Added AwsHook Import Statement

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,3 +1,4 @@
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
@@ -55,8 +56,3 @@ class StageToRedshiftOperator(BaseOperator):
             self.json_path
         )
         redshift.run(formatted_sql)
-
-
-
-
-

e6e0587b5c300ff3871842ad606781c03123af7e
John Canque <jcanque@gmail.com>
2020-12-09 10:33:22 -0500

    First Commit

---
+++ dags/test_dag_create_table.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.postgres_operator import PostgresOperator
+
+efault_args = {
+    'owner': 'johnrick',
+    'start_date': datetime(2020, 12, 1),
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=300),
+    'catchup': False
+}
+
+
+dag = DAG('test_dag_create_table',
+          default_args=default_args,
+          description='Create tables on Redshift'#,
+        #   schedule_interval='0 * * * *'
+        )
+
+table_creation = PostgresOperator(
+    task_id='tables_creation',  
+    dag=dag,
+    postgres_conn_id='redshift',
+    sql = '/create_tables.sql'
+)

d4f414c2814919498716e7fd6b12f4d90244f6d4
John Canque <jcanque@gmail.com>
2020-12-09 20:52:58 -0500

    feat: Add Dimension Table Inserts

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -56,28 +56,45 @@ load_songplays_table = LoadFactOperator(
     provide_context=True,
     redshift_conn_id="redshift",
     aws_credentials_id="aws_credentials",
+    table="songplays",
     select_query=SqlQueries.songplay_table_insert
-)
 )
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="users",
+    truncate_table=True,
+    select_query=SqlQueries.user_table_insert
 )
+
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="users",
+    truncate_table=True,
+    select_query=SqlQueries.song_table_insert
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="users",
+    truncate_table=True,
+    select_query=SqlQueries.artist_table_insert
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
-    dag=dag
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="users",
+    truncate_table=True,
+    select_query=SqlQueries.time_table_insert
 )
 
 run_quality_checks = DataQualityOperator(

2dcf610fedf4f79bd712827e13e23b149892a6ab
John Canque <jcanque@gmail.com>
2020-12-09 21:01:58 -0500

    feat: Added Execute Statement

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -17,4 +17,12 @@ class DataQualityOperator(BaseOperator):
         self.tables = tables
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        for table in self.tables:
+            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
+            if len(records) < 1 or len(records[0]) < 1:
+                raise ValueError(f"Data quality check failed, {table} returned no results")
+            num_records = records[0][0]
+            if num_records < 1:
+                raise ValueError(F"Data quality check failed, {table} contained 0 rows")
+            self.log.info(f"Data quality on table {table} check passed with {records[0][0]} records")

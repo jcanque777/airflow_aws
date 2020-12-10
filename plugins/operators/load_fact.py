1a46a77be9976a7c3041644f060533e7b01df8ec
John Canque <jcanque@gmail.com>
2020-12-09 21:19:51 -0500

    fix: Syntax Fix

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -12,19 +12,21 @@ class LoadFactOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 redshift_conn_id,
-                 table,
-                 select_query,
-                 *args, **kwargs):
+                aws_credentials_id="",
+                redshift_conn_id="",
+                table="",
+                select_query="",
+                *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
+        self.aws_credentials_id=aws_credentials_id,
         self.redshift_conn_id = redshift_conn_id
         self.table = table
         self.select_query = select_query
 
     def execute(self, context):
-        redshift = PostgresHook(postgress_conn_id=self.redshift_conn_id)
-        redshift.run(LoadFactOperator.insert_into_stmt.format(
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        redshift_hook.run(LoadFactOperator.insert_into_stmt.format(
             table=self.table,
             select_query=self.select_query
         ))

0a1bd205be07669acec8d21b80aeb6bbbee5ded6
John Canque <jcanque@gmail.com>
2020-12-10 16:28:02 -0500

    Added Truncate Statement to Operator

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -10,11 +10,16 @@ class LoadFactOperator(BaseOperator):
         {select_query}
     """
 
+    truncate_stmt = """
+        TRUNCATE TABLE {table}
+    """
+    
     @apply_defaults
     def __init__(self,
                 aws_credentials_id="",
                 redshift_conn_id="",
                 table="",
+                truncate_table="",
                 select_query="",
                 *args, **kwargs):
 
@@ -22,10 +27,17 @@ class LoadFactOperator(BaseOperator):
         self.aws_credentials_id=aws_credentials_id,
         self.redshift_conn_id = redshift_conn_id
         self.table = table
+        self.truncate_table = truncate_table
         self.select_query = select_query
 
     def execute(self, context):
         redshift_hook = PostgresHook(self.redshift_conn_id)
+        
+        if self.truncate_table:
+            redshift.run(LoadFactOperator.truncate_stmt.format(
+                table=self.table
+            ))
+        
         redshift_hook.run(LoadFactOperator.insert_into_stmt.format(
             table=self.table,
             select_query=self.select_query

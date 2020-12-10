426a2418a913531277012878d99820f37327738b
John Canque <jcanque@gmail.com>
2020-12-10 11:00:30 -0500

    fix: Matched New Format From stage_redshift

--- dags/test_dag_stage_songs_to_redshift.py
+++ dags/test_dag_stage_songs_to_redshift.py
@@ -7,7 +7,7 @@ from helpers import SqlQueries
 
 default_args = {
     'owner': 'johnrick',
-    'start_date': datetime(2020, 12, 8),
+    'start_date': datetime(2020, 12, 9),
     'depends_on_past': False,
     'retries': 0, # change back to 3 when working
     'retry_delay': timedelta(seconds=300),
@@ -16,17 +16,18 @@ default_args = {
 
 dag = DAG('test_dag_stage_songs_to_redshift',
           default_args=default_args,
-          description='Load and transform data in Redshift with Airflow' #,
+          description='Load and transform data in Redshift with Airflow',
+          max_active_runs=1
         #   schedule_interval='0 * * * *'
         )
 
-stage_events_to_redshift = StageToRedshiftOperator(
+stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='stage_songs_to_redshift_task',
     dag=dag,
-    table="staging_songs",
     redshift_conn_id="redshift",
     aws_credentials_id="aws_credentials",
+    table="staging_songs",
     s3_bucket="udacity-dend",
-    s3_key="song_data"#,
-    # json_path="s3://udacity-dend/log_json_path.json"
+    s3_key="song_data",
+    file_format = 'JSON \'auto\'' 
 )

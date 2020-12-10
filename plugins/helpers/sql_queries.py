0feedf5a33b40bf3d057eeeed003c38a9d415eb5
John Canque <jcanque@gmail.com>
2020-12-10 08:59:42 -0500

    test: Changed To Limit 25

--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -17,31 +17,31 @@ class SqlQueries:
             ON events.song = songs.title
                 AND events.artist = songs.artist_name
                 AND events.length = songs.duration
-            LIMIT 5
+            LIMIT 25
     """)
 
     user_table_insert = ("""
         SELECT distinct userid, firstname, lastname, gender, level
         FROM staging_events
         WHERE page='NextSong'
-        LIMIT 5
+        LIMIT 25
     """)
 
     song_table_insert = ("""
         SELECT distinct song_id, title, artist_id, year, duration
         FROM staging_songs
-        LIMIT 5
+        LIMIT 25
     """)
 
     artist_table_insert = ("""
         SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
         FROM staging_songs
-        LIMIT 5
+        LIMIT 25
     """)
 
     time_table_insert = ("""
         SELECT start_time, extract(hour from start_time) as hour, extract(day from start_time) as day, extract(week from start_time) as week, 
                extract(month from start_time) as month, extract(year from start_time) as year, extract(dayofweek from start_time) as weekday
         FROM songplays
-        LIMIT 5
+        LIMIT 25
     """)

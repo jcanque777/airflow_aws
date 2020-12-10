6d074e5f99c40dfbaf586ce1aee137042072767f
John Canque <jcanque@gmail.com>
2020-12-10 13:30:25 -0500

    test: Using 2nd Version of StagingOperator

--- dags/test_dag_stage_songs_to_redshift.py
+++ dags/test_dag_stage_songs_to_redshift.py
@@ -5,9 +5,11 @@ from airflow import DAG
 from airflow.operators import StageToRedshiftOperator
 from helpers import SqlQueries
 
+start_date = datetime.utcnow()
+
 default_args = {
     'owner': 'johnrick',
-    'start_date': datetime(2020, 12, 9),
+    'start_date': datetime.now(),
     'depends_on_past': False,
     'retries': 0, # change back to 3 when working
     'retry_delay': timedelta(seconds=300),
@@ -22,12 +24,12 @@ dag = DAG('test_dag_stage_songs_to_redshift',
         )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='stage_songs_to_redshift_task',
+    task_id='Stage_songs',
     dag=dag,
-    redshift_conn_id="redshift",
-    aws_credentials_id="aws_credentials",
     table="staging_songs",
-    s3_bucket="udacity-dend",
-    s3_key="song_data",
+    conn_id='redshift',
+    aws_credentials_id="aws_credentials",
+    s3_bucket='udacity-dend',
+    s3_key = 'song_data/A/A',
     file_format = 'JSON \'auto\'' 
 )
